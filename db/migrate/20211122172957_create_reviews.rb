1b12d3b2c449d50523391d321bdc0e5df8792216
Dimitri Pantelli <dimitri.pantelli@gmail.com>
2021-11-23 11:29:23 +0000

    fix user association in migrations

--- db/migrate/20211122172957_create_reviews.rb
+++ db/migrate/20211122172957_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.references :show_id, null: false, foreign_key: true
       t.float :rating
       t.text :content
