935658899105d1e42c3c9e6422947d466a538b42
Alison Clare <alisonclare93@gmail.com>
2021-11-23 16:37:40 +0000

    created user controller and route to account path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/show'
   devise_for :users
+  resources :users, only: :show
+  get 'account', to: 'user#show'
   root to: 'pages#home'
   resources :shows
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
