308c64a0ddc80e9ab85d6d3c4de045a09a2ee8df
Sofie Fiskerstrand <sofiepilskog@gmail.com>
2021-11-24 16:15:16 +0000

    small css changes

---
+++ app/javascript/packs/style_navbar.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  let homeNavbar = document.querySelector(".home-navbar")
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 500) {
+    homeNavbar.style.display = "flex";
+  } else {
+    homeNavbar.style.display = "none";
+  }
+}
