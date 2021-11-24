1008ea3f1c50e7e38a726cbfb76672a54e69fb83
Ebitari Alaibe <aebitari@gmail.com>
2021-11-24 16:21:08 +0000

    show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :shows, only: [:index, :show]
+  collection do
+    get 'details'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
