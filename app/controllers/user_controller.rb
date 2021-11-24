14cee7c00395d5836a9aeeefa280b9fa197800b8
Alison Clare <alisonclare93@gmail.com>
2021-11-24 11:41:43 +0000

    show progress on my shows page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,5 +2,7 @@ class UserController < ApplicationController
   def show
     @user = current_user
     @followed_shows = @user.followed_shows
+    @progresses = Progress.all
+    @my_progresses = @progresses.where(user_id: @user.id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+FollowedShow.destroy_all
+Episode.destroy_all
+Progress.destroy_all
+
 followed = FollowedShow.create!({
   user_id: 1,
   show_id: 1
+})
+
+episode = Episode.create!({
+  show_id: 1,
+  season_number: 2,
+  name: 'Jess does something quirky',
+  episode_number: 6
+})
+
+progress = Progress.create!({
+  user_id: 1,
+  episode_id: 1
 })
