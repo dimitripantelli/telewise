9af25a98bf03116fbe4a5e74819b0910876fa13c
Sofie Fiskerstrand <sofiepilskog@gmail.com>
2021-11-25 15:25:49 +0000

    add search page and search to navbar

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,6 @@
 class Show < ApplicationRecord
+  # searchkick word_start: ['name']
+
   validates :name, :summary, :number_of_seasons, presence: true
   has_many :episodes
   has_one_attached :photo
