2cbf2513fc4a6013225340e102668f2836775e4d
Alison Clare <alisonclare93@gmail.com>
2021-11-26 10:48:11 +0000

    creating method in controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,13 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user = current_user
+    @notifications = Notification.where(user: current_user).unread
+    @followed_shows = @user.followed_shows
+    @followed_shows.each do |show|
+      p show.show.episodes
+    end
+    Notification.create(recipient_id: @user)
+  end
 end
