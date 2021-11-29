6dd2fa182e32d6ed05dd12a8a234a52799a47193
Dimitri Pantelli <dimitri.pantelli@gmail.com>
2021-11-29 14:38:41 +0000

    Checkboxes check all previous episodes on the page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,11 +15,20 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "controllers"
-import "bootstrap"
+import "controllers";
+import "bootstrap";
 
 // import { scrollFunction } from 'style_navbar.js'
 
 // document.addEventListener('turbolinks:load', () => {
 //   window.onscroll = function() {scrollFunction()};
 // }
+
+window.checkPrevious = function (currCheckbox) {
+  let allPrevious = Array.from(
+    document.getElementsByClassName("episode-check")
+  ).filter((otherCheckbox) => {
+    return parseInt(otherCheckbox.id,10) < parseInt(currCheckbox.id,10);
+  });
+  allPrevious.forEach((input) => input.checked = true);
+};
