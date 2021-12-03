7d8bba2c0e3a786d9cacb66009c25e769a77f344
Dimitri Pantelli <dimitri.pantelli@gmail.com>
2021-12-03 09:32:34 +0000

    Seed file with 9 TV shows

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,89 +7,89 @@ require 'openssl'
 # Episode.destroy_all
 # SeasonSummary.destroy_all
 
-# show_ids = %w[23470 175 169 73 525 58 32 527 139]
+show_ids = %w[43687 2705 28276 299 56942 3594 26856 39892 1871064]
 
-# puts 'Adding shows and episodes...'
+puts 'Adding shows and episodes...'
 
-# show_ids.each do |id|
-#   # API requests
-#   show = JSON.parse(URI.open("https://api.tvmaze.com/shows/#{id}?embed=seasons").read)
-#   episodes = JSON.parse(URI.open("https://api.tvmaze.com/shows/#{id}/episodes").read)
-#   imdb_id = show['externals']['imdb']
-#   url_gb = URI("https://streaming-availability.p.rapidapi.com/get/basic?country=gb&imdb_id=#{imdb_id}&output_language=en")
-#   url_us = URI("https://streaming-availability.p.rapidapi.com/get/basic?country=us&imdb_id=#{imdb_id}&output_language=en")
+show_ids.each do |id|
+  # API requests
+  show = JSON.parse(URI.open("https://api.tvmaze.com/shows/#{id}?embed=seasons").read)
+  episodes = JSON.parse(URI.open("https://api.tvmaze.com/shows/#{id}/episodes").read)
+  imdb_id = show['externals']['imdb']
+  url_gb = URI("https://streaming-availability.p.rapidapi.com/get/basic?country=gb&imdb_id=#{imdb_id}&output_language=en")
+  url_us = URI("https://streaming-availability.p.rapidapi.com/get/basic?country=us&imdb_id=#{imdb_id}&output_language=en")
 
-#   http_gb = Net::HTTP.new(url_gb.host, url_gb.port)
-#   http_gb.use_ssl = true
-#   http_gb.verify_mode = OpenSSL::SSL::VERIFY_NONE
-#   http_us = Net::HTTP.new(url_us.host, url_us.port)
-#   http_us.use_ssl = true
-#   http_us.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  http_gb = Net::HTTP.new(url_gb.host, url_gb.port)
+  http_gb.use_ssl = true
+  http_gb.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  http_us = Net::HTTP.new(url_us.host, url_us.port)
+  http_us.use_ssl = true
+  http_us.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-#   request_gb = Net::HTTP::Get.new("/get/basic?country=gb&imdb_id=#{imdb_id}&output_language=en")
-#   request_gb["x-rapidapi-host"] = 'streaming-availability.p.rapidapi.com'
-#   request_gb["x-rapidapi-key"] = ENV["RAPID_API_KEY"]
-#   request_us = Net::HTTP::Get.new("/get/basic?country=us&imdb_id=#{imdb_id}&output_language=en")
-#   request_us["x-rapidapi-host"] = 'streaming-availability.p.rapidapi.com'
-#   request_us["x-rapidapi-key"] = ENV["RAPID_API_KEY"]
+  request_gb = Net::HTTP::Get.new("/get/basic?country=gb&imdb_id=#{imdb_id}&output_language=en")
+  request_gb["x-rapidapi-host"] = 'streaming-availability.p.rapidapi.com'
+  request_gb["x-rapidapi-key"] = ENV["RAPID_API_KEY"]
+  request_us = Net::HTTP::Get.new("/get/basic?country=us&imdb_id=#{imdb_id}&output_language=en")
+  request_us["x-rapidapi-host"] = 'streaming-availability.p.rapidapi.com'
+  request_us["x-rapidapi-key"] = ENV["RAPID_API_KEY"]
 
-#   p response_gb = JSON.parse(http_gb.request(request_gb).read_body)
-#   response_us = JSON.parse(http_us.request(request_us).read_body)
+  response_gb = JSON.parse(http_gb.request(request_gb).read_body)
+  response_us = JSON.parse(http_us.request(request_us).read_body)
 
-#   streaming = []
-#   streaming << response_gb['streamingInfo']
-#   if response_gb['streamingInfo'] && response_gb['streamingInfo'].first[0] != response_us['streamingInfo'].first[0]
-#     streaming << response_us['streamingInfo']
-#   end
+  streaming = []
+  streaming << response_gb['streamingInfo']
+  if response_gb['streamingInfo'] && response_gb['streamingInfo'].first[0] != response_us['streamingInfo'].first[0]
+    streaming << response_us['streamingInfo']
+  end
 
-#   # Create show using JSON
-#   new_show = Show.create!(
-#     name: show['name'],
-#     summary: response_gb['overview'],
-#     number_of_seasons: show['_embedded']['seasons'].count,
-#     streaming: streaming,
-#     rating: show['rating']['average']
-#   )
+  # Create show using JSON
+  new_show = Show.create!(
+    name: show['name'],
+    summary: response_gb['overview'],
+    number_of_seasons: show['_embedded']['seasons'].count,
+    streaming: streaming,
+    rating: show['rating']['average']
+  )
 
-#   # Add poster to asset file
-#   download = URI.open(show['image']['original'])
-#   IO.copy_stream(download, "app/assets/images/#{show['name'].gsub(/\s+/, '')}.jpg")
+  # Add poster to asset file
+  download = URI.open(show['image']['original'])
+  IO.copy_stream(download, "app/assets/images/#{show['name'].gsub(/\s+/, '')}.jpg")
 
-#   # Check new show was saved
-#   next unless new_show.persisted?
+  # Check new show was saved
+  next unless new_show.persisted?
 
-#   # Add season summaries
-#   show['_embedded']['seasons'].each do |season|
-#     new_show.season_summaries.create!(
-#       season_number: season['number'],
-#       summary: season['summary']
-#     )
-#   end
+  # Add season summaries
+  show['_embedded']['seasons'].each do |season|
+    new_show.season_summaries.create!(
+      season_number: season['number'],
+      summary: season['summary']
+    )
+  end
 
-#   # For that show, associate these episodes
-#   episodes.each do |ep|
-#     new_show.episodes.create!(
-#       episode_number: ep['number'],
-#       season_number: ep['season'],
-#       name: ep['name'],
-#       description: ep['summary'],
-#       airing_date: ep['airdate']
-#     )
-#   end
+  # For that show, associate these episodes
+  episodes.each do |ep|
+    new_show.episodes.create!(
+      episode_number: ep['number'],
+      season_number: ep['season'],
+      name: ep['name'],
+      description: ep['summary'],
+      airing_date: ep['airdate']
+    )
+  end
 
-#   puts "added #{new_show.name}"
-# end
+  puts "added #{new_show.name}"
+end
 
-# puts '...Finished!'
+puts '...Finished!'
 
 # TEMP NOTIFICATIONS
 
-puts "Changing an episode of Succession to today's date"
-succession = Show.find_by(name: 'Succession')
-today_episode = succession.episodes.find(5617)
-today_episode.airing_date = Date.today
-today_episode.save
-puts "Done :)"
+# puts "Changing an episode of Succession to today's date"
+# succession = Show.find_by(name: 'Succession')
+# today_episode = succession.episodes.find(5617)
+# today_episode.airing_date = Date.today
+# today_episode.save
+# puts "Done :)"
 
 
 # TEMP FOLLWED SHOWS
