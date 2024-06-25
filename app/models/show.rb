c1d99ae660f5eace2c46a7bf25c6b456f9673fa7
Ebitari Alaibe <aebitari@gmail.com>
2024-06-25 17:44:06 +0100

    update to telewise

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,7 @@ class Show < ApplicationRecord
   # searchkick word_start: ['name']
 
   validates :name, :summary, :number_of_seasons, presence: true
+  has_many :reviews, dependent: :destroy
   has_many :episodes, dependent: :destroy
   has_many :season_summaries, dependent: :destroy
   has_many :followed_shows, dependent: :destroy
