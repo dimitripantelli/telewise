bb8f9fa7a41f5270f36c5039021c10e9943417c9
Ebitari Alaibe <91223087+Ebitari-A@users.noreply.github.com>
2024-06-25 17:49:16 +0100

    Revert "update to telewise"

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,6 @@ class Show < ApplicationRecord
   # searchkick word_start: ['name']
 
   validates :name, :summary, :number_of_seasons, presence: true
-  has_many :reviews, dependent: :destroy
   has_many :episodes, dependent: :destroy
   has_many :season_summaries, dependent: :destroy
   has_many :followed_shows, dependent: :destroy
